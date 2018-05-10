27bc5f889fc7d1f13f835ef8e18dafd9b207b53a
EGiadans <A01411416@itesm.mx>
2018-05-10 09:39:16 -0500

    Final Commit

---
+++ Assets/Game/Scripts/Button.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class Button : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+    }
+    //Allow the game to continue when pressed
+    public void BotonPresionadoStart()
+    {
+        Debug.Log("presiono start");
+        Time.timeScale = 1;
+
+    }
+    //Terminate application
+    public void BotonPresionadoQuit()
+    {
+        Debug.Log("presiono quit");
+        Application.Quit();
+
+    }
+}
---
+++ Assets/Game/Scripts/NewManejador.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NewManejador : MonoBehaviour
+{
+
+    public Canvas preguntas;
+    private Completed.Player player;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        preguntas = GameObject.Find("StartMenu").GetComponent<Canvas>();
+        player = GameObject.FindObjectOfType<Completed.Player>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    //Allow the game to be played with timescale = 1
+    public void BotonPresionadoStart()
+    {
+        Debug.Log("presiono start");
+        
+        Time.timeScale = 1;
+        
+    }
+    //End application
+    public void BotonPresionadoQuit()
+    {
+        Debug.Log("presiono quit");
+        Application.Quit();
+        
+    }
+
+}
